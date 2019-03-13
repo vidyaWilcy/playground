16dc37141428d8ac8c9cd512999b1121ccfa1071
Vidya Wilcy <vidyawilcy20@gmail.com>
2019-03-13 10:37:09 +0530

    assignment 4

---
+++ third party modules/2.js
@@ -0,0 +1,5 @@
+const moment=require('moment');
+let now=moment();
+console.log(now.format());
+console.log(now.date());
+console.log(now.week());
---
+++ third party modules/demo.js
@@ -0,0 +1,6 @@
+var lodash=require('lodash');
+
+console.log(lodash.isString("abc"));
+console.log(lodash.sum([2,4,6,8]));
+console.log(lodash.isNumber(123));
+console.log(lodash.min([2,10,4]));
