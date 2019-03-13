bfa8105c347c3a29705dd2da14eb83c74c696b8c
Vidya Wilcy <vidyawilcy20@gmail.com>
2019-03-13 10:13:24 +0530

    assignment1
    hello world

--- core modules/append.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-
-fs.appendFile('append.txt', 'email id-vidyawilcy20@gmail.com', function (err) {
-  if (err) throw err;
-  console.log('succesfully updated with the text');
-});
--- core modules/write.js
+++
@@ -1,6 +0,0 @@
-const fs=require("fs");
-
-fs.writeFile("write.txt", "HELLOOOOOOOOoooo", function (err) {
-  if (err) throw err;
-console.log("successfully written to file");
-})
